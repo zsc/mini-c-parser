68e483ae0ea8f327a165acfbd858ea19b2dd2280
Shuchang Zhou <zsc@wuluedeMacBook-Air.local>
2025-07-13 12:22:13 +0800

    fix

--- bin/main.ml
+++ bin/main.ml
@@ -18,12 +18,17 @@ type expr =
   | Id of string                    (* 标识符/变量: e.g., n *)
   | BinOp of binop * expr * expr    (* 二元运算: e.g., n * fac(n-1) *)
   | Call of string * expr list      (* 函数调用: e.g., fac(4) *)
+  | ArrayAccess of expr * expr      (* 数组元素访问: e.g., arr[index] *)
 
 (* 语句类型 *)
 type stmt =
   | Return of expr                  (* return 语句: e.g., return 1; *)
   | If of expr * stmt * stmt option (* if (cond) { then } else { else } *)
   | Block of stmt list              (* 代码块: { stmt1; stmt2; ... } *)
+  | Decl of string * string * expr option (* 变量声明: e.g., int x; int x = 5; *)
+  | ArrayDecl of string * string * expr (* 数组声明: e.g., int arr[10]; (size expr is required for local arrays) *)
+  | Assign of expr * expr           (* 赋值语句: e.g., x = 5; arr[0] = 10; *)
+  | ExprStmt of expr                (* 表达式作为语句: e.g., func(); *)
 
 (* 顶层定义 (目前只有函数) *)
 type top_level_def = {
@@ -49,6 +54,7 @@ type token =
   | T_Le | T_Eq | T_Ne | T_Lt | T_Gt | T_Ge
   (* Delimiters *)
   | T_Lparen | T_Rparen (* ( ) *)
+  | T_Assign (* = *)
   | T_Lbrace | T_Rbrace (* { } *)
   | T_Comma
   | T_Semicolon
@@ -86,6 +92,7 @@ let token_specs = [
   (Str.regexp ">", fun _ -> Some T_Gt);
   (Str.regexp "+", fun _ -> Some T_Plus);
   (Str.regexp "-", fun _ -> Some T_Minus);
+  (Str.regexp "=", fun _ -> Some T_Assign); (* Must be after ==, <=, >= *)
   (Str.regexp "*", fun _ -> Some T_Star);
   (Str.regexp "/", fun _ -> Some T_Slash);
 
@@ -94,6 +101,8 @@ let token_specs = [
   (Str.regexp ")", fun _ -> Some T_Rparen);
   (Str.regexp "{", fun _ -> Some T_Lbrace);
   (Str.regexp "}", fun _ -> Some T_Rbrace);
+  (Str.regexp "\\[", fun _ -> Some T_Lbracket);
+  (Str.regexp "\\]", fun _ -> Some T_Rbracket);
   (Str.regexp ",", fun _ -> Some T_Comma);
   (Str.regexp ";", fun _ -> Some T_Semicolon);
 ]
@@ -127,7 +136,8 @@ let expect token tokens =
   | t :: _ ->
       failwith (Printf.sprintf "Parser error: Expected %s but found %s"
         (match token with
-         | T_Lparen -> "'('" | T_Rparen -> "')'" | T_Lbrace -> "'{'"
+         | T_Lparen -> "'('" | T_Rparen -> "')'" | T_Lbracket -> "'['"
+         | T_Rbracket -> "']'" | T_Lbrace -> "'{'" | T_Assign -> "'='"
          | T_Rbrace -> "'}'" | T_Comma -> "','" | T_Int -> "'int'"
          | T_Semicolon -> "';'"
          | _ -> "token")
@@ -154,19 +164,25 @@ and parse_top_level_def tokens =
   let rest3 = expect T_Lparen rest2 in
   let params, rest4 = parse_params rest3 in
   let rest5 = expect T_Rparen rest4 in
-  let body, rest6 = parse_stmt rest5 in
+  let body, rest6 = parse_stmt rest5 in (* Function body must be a block *)
   ({ ret_type = "int"; name; params; body }, rest6)
+
+and parse_type_name tokens =
+  match tokens with
+  | T_Int :: T_Lbracket :: T_Rbracket :: rest -> ("int[]", rest)
+  | T_Int :: rest -> ("int", rest)
+  | _ -> failwith "Expected type name (e.g., 'int' or 'int[]')"
 
 and parse_params tokens =
   match tokens with
   | T_Rparen :: _ -> ([], tokens) (* No parameters *)
   | _ ->
       let rec loop acc tokens =
-        let rest1 = expect T_Int tokens in
+        let (param_type, rest1) = parse_type_name tokens in
         let name, rest2 = match rest1 with
           | T_Id s :: rest -> s, rest
           | _ -> failwith "Expected parameter name" in
-        let new_acc = acc @ [("int", name)] in
+        let new_acc = acc @ [(param_type, name)] in
         match rest2 with
         | T_Comma :: rest' -> loop new_acc rest'
         | _ -> (new_acc, rest2)
@@ -200,7 +216,41 @@ and parse_stmt tokens =
             let (stmt, t') = parse_stmt t in
             parse_stmts (stmt :: acc) t'
       in
-      parse_stmts [] rest
+      parse_stmts [] rest (* Start parsing statements within the block *)
+  (* New statement types: Decl, ArrayDecl, Assign, ExprStmt *)
+  | T_Int :: _ -> (* Potential variable or array declaration *)
+      let (decl_type, rest_type) = parse_type_name tokens in (* consumes T_Int, potentially T_Lbracket T_Rbracket *)
+      let name, rest_name = match rest_type with
+        | T_Id s :: rest -> s, rest
+        | _ -> failwith "Expected identifier after type in declaration" in
+      (match rest_name with
+       | T_Lbracket :: rest_idx -> (* Array declaration: int arr[size]; *)
+           let (size_expr, rest_idx') = parse_expr rest_idx in
+           let rest_idx'' = expect T_Rbracket rest_idx' in
+           let rest_final = expect T_Semicolon rest_idx'' in
+           (ArrayDecl (decl_type, name, size_expr), rest_final)
+       | T_Assign :: rest_assign -> (* Scalar declaration with initialization: int x = 5; *)
+           let (initial_expr, rest_expr) = parse_expr rest_assign in
+           let rest_final = expect T_Semicolon rest_expr in
+           (Decl (decl_type, name, Some initial_expr), rest_final)
+       | T_Semicolon :: rest_final -> (* Scalar declaration without initialization: int x; *)
+           (Decl (decl_type, name, None), rest_final)
+       | _ -> failwith "Expected '=', '[', or ';' after identifier in declaration"
+      )
+  | T_Id _ -> (* Assignment or function call as statement *)
+      (* Try to parse the expression which could be an L-value or a Call *)
+      let (first_expr, rest_after_expr) = parse_expr tokens in
+      (match rest_after_expr with
+       | T_Assign :: rest_assign ->
+           let (rhs_expr, rest_rhs) = parse_expr rest_assign in
+           let rest_final = expect T_Semicolon rest_rhs in
+           (Assign (first_expr, rhs_expr), rest_final)
+       | T_Semicolon :: rest_final ->
+           (* Only function calls, identifiers, or array accesses can be standalone expression statements. *)
+           (match first_expr with
+            | Call _ | Id _ | ArrayAccess _ -> (ExprStmt first_expr, rest_final)
+            | _ -> failwith "Parser error: Only function calls, identifiers, or array accesses can be standalone expression statements." )
+       | _ -> failwith "Parser error: Expected '=' or ';' after expression" )
   | _ -> failwith "Parser error: Unexpected token in statement"
 
 (* 表达式解析 (带操作符优先级) *)
@@ -272,29 +322,41 @@ and parse_primary_expr tokens =
   match tokens with
   | T_Num n :: rest -> (Cst n, rest)
   | T_Id s :: rest ->
-      (match rest with
-       | T_Lparen :: _ -> parse_call_expr s rest
-       | _ -> (Id s, rest))
+      (* This 's' could be a function name or an array name or a variable name *)
+      (* parse_postfix_expr will consume (args) or [index] if present *)
+      parse_postfix_expr (Id s) rest
   | T_Lparen :: rest ->
       let (expr, rest') = parse_expr rest in
       let rest'' = expect T_Rparen rest' in
       (expr, rest'')
   | _ -> failwith "Parser error: Unexpected token in expression"
 
-and parse_call_expr name tokens =
-  let rest1 = expect T_Lparen tokens in
-  match rest1 with
-  | T_Rparen :: rest' -> (Call (name, []), rest') (* No arguments *)
-  | _ ->
-      let rec loop acc toks =
-        let (arg, toks') = parse_expr toks in
-        let new_acc = acc @ [arg] in
-        match toks' with
-        | T_Comma :: rest -> loop new_acc rest
-        | T_Rparen :: rest -> (Call (name, new_acc), rest)
-        | _ -> failwith "Parser error: Expected ',' or ')' in argument list"
+(* This function takes a 'base' expression (e.g., an Id) and applies
+   postfix operations like function calls or array accesses. *)
+and parse_postfix_expr current_expr tokens =
+  match tokens with
+  | T_Lparen :: _ -> (* This is a function call *)
+      let func_name = match current_expr with
+        | Id s -> s
+        | _ -> failwith "Parser error: Function name must be an identifier"
       in
-      loop [] rest1
+      let rest1 = expect T_Lparen tokens in
+      let (args, rest2) = parse_args rest1 in (* Helper to parse argument list *)
+      let rest3 = expect T_Rparen rest2 in
+      let call_expr = Call (func_name, args) in
+      parse_postfix_expr call_expr rest3 (* Allows f()() or f()[0] *)
+  | T_Lbracket :: rest -> (* This is an array access *)
+      let (index_expr, rest') = parse_expr rest in
+      let rest'' = expect T_Rbracket rest' in
+      let array_access_expr = ArrayAccess (current_expr, index_expr) in
+      parse_postfix_expr array_access_expr rest'' (* Allows arr[i][j] *)
+  | _ -> (current_expr, tokens) (* No more postfix operations *)
+
+(* Helper for parsing argument list of a function call *)
+and parse_args tokens =
+  match tokens with
+  | T_Rparen :: _ -> ([], tokens) (* No arguments, Rparen is next *)
+  | _ -> let rec loop acc toks = let (arg, toks') = parse_expr toks in let new_acc = acc @ [arg] in match toks' with | T_Comma :: rest -> loop new_acc rest | _ -> (new_acc, toks') in loop [] tokens
 
 (* IV. AST Pretty Printer (for testing) *)
 
@@ -311,7 +373,9 @@ let rec string_of_expr = function
   | Call (name, args) ->
       let args_str = String.concat ", " (List.map string_of_expr args) in
       Printf.sprintf "%s(%s)" name args_str
-
+  | ArrayAccess (base, index) ->
+      Printf.sprintf "%s[%s]" (string_of_expr base) (string_of_expr index)
+      
 let rec string_of_stmt indent = function
   | Return e -> indent ^ "Return " ^ (string_of_expr e)
   | If (cond, then_s, else_s_opt) ->
@@ -325,6 +389,19 @@ let rec string_of_stmt indent = function
       indent ^ "{\n" ^
       (String.concat "\n" (List.map (string_of_stmt (indent ^ "  ")) stmts)) ^
       "\n" ^ indent ^ "}"
+  | Decl (typ, name, init_opt) ->
+      let init_str = match init_opt with
+        | Some e -> Printf.sprintf " = %s" (string_of_expr e)
+        | None -> ""
+      in
+      Printf.sprintf "%sDecl %s %s%s;" indent typ name init_str
+  | ArrayDecl (typ, name, size_expr) ->
+      Printf.sprintf "%sArrayDecl %s %s[%s];" indent typ name (string_of_expr size_expr)
+  | Assign (lhs, rhs) ->
+      Printf.sprintf "%sAssign %s = %s;" indent (string_of_expr lhs) (string_of_expr rhs)
+  | ExprStmt e ->
+      Printf.sprintf "%sExprStmt %s;" indent (string_of_expr e)
+
 
 let string_of_def (d : top_level_def) =
   let params_str = String.concat ", " (List.map (fun (t, n) -> t ^ " " ^ n) d.params) in
@@ -348,15 +425,26 @@ let parse_from_string str : (program, string) result =
 (* VI. Test in Main *)
 let () =
   let input_code = "
-    int fac(int n) {
-      if (n <= 1) {
-        return 1;
+    int get_element(int[] arr, int index) {
+      if (index < 0) {
+        return -1; // Error for negative index
       }
-      return n * fac(n-1);
+      return arr[index];
     }
 
     int main() {
-        return fac(4);
+      int my_var = 10; // Scalar declaration with initialization
+      int my_array[3]; // Array declaration
+      int x; // Scalar declaration
+
+      my_array[0] = 100; // Array element assignment
+      my_array[1] = 200;
+      my_array[2] = 300;
+
+      x = my_array[0] + my_array[1]; // Assignment with array access
+      x = x + get_element(my_array, 2); // Function call with array parameter
+
+      return x;
     }
   " in
 
